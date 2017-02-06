5f702adb17ab2fa158ab86c790cc4050a015ea60
Karthik Venkataraman <kvenkata1974@gmail.com>
2017-02-06 17:37:17 +1100

    Tally score done

--- src/main/java/vodafone/exam/bowling/tenpin/Bowling.java
+++ src/main/java/vodafone/exam/bowling/tenpin/Bowling.java
@@ -1,6 +1,7 @@
 package vodafone.exam.bowling.tenpin;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -13,7 +14,7 @@ public class Bowling {
     public final static String RANGE_ERROR = "Score outside of range";
 
     private List<Integer> scores;
-    private int frameCount = 0;
+
     private List<Frame> frames;
 
 
@@ -43,7 +44,7 @@ public class Bowling {
 
                 // Check if strike and current frame is not the last frame
                 if (currentFrame.isStrike() && frames.size() < MAX_FRAMES-1) {
-                    // Roll over to new frame in next iteration as boolean is still true
+                    // Roll over to new frame in next iteration as boolean "newFrame" is still true
                     addFrame(currentFrame);
                 } else {
                     newFrame = Boolean.FALSE;
@@ -52,7 +53,7 @@ public class Bowling {
                 scoreIndex++;
                 currentFrame.setScore(scoreIndex, i);
 
-                // Frame roll over check
+                // Frame roll over check, last frame can have 3 score entries in case of strike or spare
                 if (frames.size() < MAX_FRAMES-1 ||
                         (scoreIndex == Frame.MAX_SCORE_INDEX || !currentFrame.isStrike() && !currentFrame.isSpare())) {
                     addFrame(currentFrame);
@@ -61,6 +62,11 @@ public class Bowling {
 
             }
 
+        }
+
+        if (!newFrame) {
+            // Incomplete frame due to lack of entry(s)
+            addFrame(currentFrame);
         }
     }
 
@@ -74,9 +80,73 @@ public class Bowling {
 
     }
 
+    public int tallyFrames() {
+
+        int tally = 0;
+
+        for (int i = 0; i < frames.size(); i++) {
+            tally += tallyFrame(i);
+        }
+
+        return tally;
+    }
+
+    private int tallyFrame(int frameNumber) {
+
+        int tally = 0;
+
+        if (frameNumber < frames.size()) {
+
+            Frame currentFrame = frames.get(frameNumber);
+
+            // Last frame is a special case
+            if (frameNumber == MAX_FRAMES -1) {
+                if (currentFrame.isStrike()) {
+                    tally = Frame.MAX_SCORE + currentFrame.getScore(1) + currentFrame.getScore(2);
+                } else if (currentFrame.isSpare()) {
+                    tally = Frame.MAX_SCORE + currentFrame.getScore(2);
+                } else
+                    tally = currentFrame.getScore(0) + currentFrame.getScore(1);
+            } else { // Not last frame
+                Frame zeroFrame = new Frame();
+
+                Frame nextFrame = ((frameNumber + 1) < frames.size()) ? frames.get(frameNumber + 1) : zeroFrame;
+                Frame nextNextFrame = ((frameNumber + 2) < frames.size()) ? frames.get(frameNumber + 2) : zeroFrame;
+
+                if (currentFrame.isStrike()) {
+                    if (nextFrame.isStrike()) {
+                        // Special case for last but one frame, use the second slot of last frame
+                        if (frameNumber == MAX_FRAMES -2) {
+                            tally = (Frame.MAX_SCORE * 2) + nextFrame.getScore(1);
+                        } else {
+                            tally = (Frame.MAX_SCORE * 2) + nextNextFrame.getScore(0);
+                        }
+
+                    } else if (nextFrame.isSpare()) {
+                        tally = Frame.MAX_SCORE * 2;
+                    } else {
+                        tally = Frame.MAX_SCORE + nextFrame.getScore(0) + nextFrame.getScore(1);
+                    }
+
+                } else if (currentFrame.isSpare()) {
+                    tally = Frame.MAX_SCORE + nextFrame.getScore(0);
+                } else
+                    tally = currentFrame.getScore(0) + currentFrame.getScore(1);
+            }
+
+
+        }
+
+        System.out.println("Frame " + frameNumber + " = " + tally);
+        return tally;
+    }
+
     public static void main (String[] args) {
 
         Bowling bowling = new Bowling(args);
+        int tally = bowling.tallyFrames();
+
+        System.out.println(Arrays.toString(args) + " --> " + tally);
 
     }
 }
